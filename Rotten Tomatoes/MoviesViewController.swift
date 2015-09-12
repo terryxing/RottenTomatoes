418cda03d68e2f30999dc8428f2abfc6f93dabff
xingtianyi <XINGTIANYI@gmail.com>
2015-09-11 21:05:46 -0700

    title can be displayed on the table view

---
+++ Rotten Tomatoes/MoviesViewController.swift
@@ -0,0 +1,89 @@
+//
+//  MoviesViewController.swift
+//  Rotten Tomatoes
+//
+//  Created by Tianyi Xing on 9/11/15.
+//  Copyright © 2015 Tianyi Xing. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+   @IBOutlet weak var tableView: UITableView!
+  
+   var movies: [NSDictionary]?
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+      tableView.dataSource = self
+      tableView.delegate = self
+      
+         let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/e41513a57049e21bc6cf/raw/b490e79be2d21818f28614ec933d5d8f467f0a66/gistfile1.json")!
+      
+      
+          let request = NSURLRequest(URL: url)
+  
+      
+      NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+        let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(data!, options:[]) as? NSDictionary
+        
+        //  NSLog("the length of the photos NSArray is: \(self.photos?.count)")
+        //  NSLog("response: \(self.photos)")
+        
+        
+        if let json = responseDictionary {
+          self.movies = json["movies"] as? [NSDictionary]
+          self.tableView.reloadData()
+        }
+        
+        //print(self.movies)
+
+      }
+  
+     }
+ 
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as UITableViewCell
+   
+    
+    let movie = self.movies![indexPath.row]
+    
+    cell.textLabel?.text = movie["title"] as? String
+    
+    return cell
+    
+    
+    
+    
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    if let movies = movies {
+      return movies.count
+    } else {
+      return 0
+    }
+
+  }
+  
+  
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
