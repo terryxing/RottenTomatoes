08f4e95d61dd5643ada342af18013ecefa344d2f
xingtianyi <xingtianyi@gmail.com>
2015-09-13 20:43:49 -0700

    search bar works

--- Rotten Tomatoes/MoviesViewController.swift
+++ Rotten Tomatoes/MoviesViewController.swift
@@ -12,14 +12,19 @@ import SwiftLoader
 import Darwin
 
 
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
    @IBOutlet weak var tableView: UITableView!
+   @IBOutlet weak var searchBar: UISearchBar!
   
    var movies: [NSDictionary]?
-  
+   var filtered:[String] = []
+   var searchActive : Bool = false
    var refreshControl:UIRefreshControl!
-
+   var titleList: [String] = []
+    var titleAndIndex = [String: Int]()
+    var filteredMovie: [NSDictionary] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
       
@@ -33,13 +38,16 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
      
 
       UITabBar.appearance().barTintColor = UIColor.blackColor()
-
+      searchBar.barTintColor = UIColor.blackColor()
+      searchBar.tintColor = UIColor.orangeColor()
+        
       
       self.navigationController?.navigationBar.barTintColor = UIColor.blackColor()
       self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
       
       tableView.dataSource = self
       tableView.delegate = self
+      searchBar.delegate = self
       
          let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/e41513a57049e21bc6cf/raw/b490e79be2d21818f28614ec933d5d8f467f0a66/gistfile1.json")!
       
@@ -68,6 +76,16 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         if let json = responseDictionary {
           self.movies = json["movies"] as? [NSDictionary]
+            var currCNT: Int = 0;
+            for movie in self.movies! {
+                self.titleList.append(movie["title"] as! String)
+                self.titleAndIndex[movie["title"] as! String] = currCNT
+                currCNT++
+            }
+            
+            NSLog("\(self.titleAndIndex)")
+            NSLog("\(self.titleList)")
+            
           sleep(1)
           SwiftLoader.hide()
           self.tableView.reloadData()
@@ -79,8 +97,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         self.refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
         self.refreshControl.addTarget(self, action: "refresh:", forControlEvents: UIControlEvents.ValueChanged)
         self.tableView.addSubview(self.refreshControl)
-
-        
         
       }
   
@@ -109,7 +125,19 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
    
     //cell.layoutMargins = UIEdgeInsetsZero
     
-    let movie = self.movies![indexPath.row]
+    var currIndex = indexPath.row
+    
+    if(searchActive) {
+
+        
+//        currIndex = 4
+        let currTitle = self.filtered[indexPath.row]
+        currIndex = titleAndIndex[currTitle]!
+        NSLog("current index for big heor is : \(currIndex)")
+  
+    }
+    
+    let movie = self.movies![currIndex]
     
     cell.titleLabel.text = movie["title"] as? String
     cell.synopsisLabel.text = movie["synopsis"] as? String
@@ -118,7 +146,10 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     let url = NSURL(string: movie.valueForKeyPath("posters.thumbnail") as! String)!
     
     cell.posterView.setImageWithURL(url)
-    return cell
+
+    
+    
+       return cell
    
   }
   
@@ -140,12 +171,17 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
   
   
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    if let movies = movies {
-      return movies.count
+    
+    if(searchActive) {
+        return filtered.count
     } else {
-      return 0
+  
+      if let movies = movies {
+        return movies.count
+      } else {
+        return 0
+      }
     }
-
   }
   
   
@@ -158,6 +194,44 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchActive = true;
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        filtered = titleList.filter({ (text) -> Bool in
+            let tmp: NSString = text
+            let range = tmp.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
+            return range.location != NSNotFound
+        })
+        if(filtered.count == 0){
+            searchActive = false;
+        } else {
+            searchActive = true;
+            
+            
+            
+            
+        }
+        self.tableView.reloadData()
+    }
+
 
 
     // MARK: - Navigation
